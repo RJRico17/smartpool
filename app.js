49168785c57fa5e1b8d5f41a584ba70c007c3b22
RJ <fredrickalexander1727@gmail.com>
2025-03-04 19:53:01 -0800

    my god alot of stuff

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ import express from "express";
 const app = express();
 app.use(express.static('public'));
 app.use(express.static('view'));
+app.set('view engine','ejs');
 app.use(express.urlencoded({extended: true}));
 
 const PORT = 4000;
 
 app.listen(PORT, (req,res) => {
     console.log(`Server is running at http://localhost:${PORT}`);
-})
+});
+app.get('/', (req,res) => {
+    res.sendFile(`${import.meta.dirname}/view/home.html`);
+});
 
-app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/view/index.html`);
-})
+app.get('/scorebug', (req,res) => {
+    res.sendFile(`${import.meta.dirname}/view/scorebug.html`)
+});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(`${import.meta.dirname}/view/scorebug.html`);
+// });
